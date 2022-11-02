dee064e9c1d968a16539d433c098c5d44f332a70
TemoanaCummings <cummings.temoana@gmail.com>
2022-11-02 21:07:55 +1300

    Completed: Even or Odd. Easy Kata

---
+++ kyu8/even-or-odd.js
@@ -0,0 +1,12 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// My Solution:
+function evenOrOdd(number) {
+  if(number % 2 === 0){
+    console.log("Even")
+  }
+  else{
+    console.log("Odd")
+  }
+}
+evenOrOdd(2)
