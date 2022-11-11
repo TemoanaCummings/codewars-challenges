9e3359afb4b210cc2e4561f78b7747dd7a2828c2
TemoanaCummings <cummings.temoana@gmail.com>
2022-11-11 19:35:30 +1300

    Attemped with better solution

--- kyu8/abbreviate-two-word-names.js
+++ kyu8/abbreviate-two-word-names.js
@@ -6,14 +6,18 @@
 // patrick feeney => P.F
 
 // My solution:
-function abbrevName(name){
-    const fullName = name.split(' ');
-    const initials = fullName.shift().charAt(0) + "." + fullName.pop().charAt(0);
-    console.log(initials.toUpperCase())
-}
-abbrevName("sam harris")
+// function abbrevName(name){
+//     const fullName = name.split(' ');
+//     const initials = fullName.shift().charAt(0) + "." + fullName.pop().charAt(0);
+//     console.log(initials.toUpperCase())
+// }
+// abbrevName("sam harris")
 
-// Better solution:
+// Second try:
 function abbrevName(name){
-    return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+    return name.split(' ').map(index => index[0].toUpperCase()).join('.')
+    // Splits name at the space inbetween
+    // map gets eletements at index position 0 and upper cases them
+    // Joins together with '.' in between
 }
+console.log(abbrevName('sam harris'));
