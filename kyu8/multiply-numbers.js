29e2978dfeb198a1f9bec881e6e421111229ef93
TemoanaCummings <cummings.temoana@gmail.com>
2022-11-17 14:54:18 +1300

    Finished challenge

---
+++ kyu8/multiply-numbers.js
@@ -0,0 +1,11 @@
+// Implement a function called multiply, which takes two numbers and returns their product:
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+// My Solution:
+function multiply(n1,n2){
+    return n1 * n2;
+}
+multiply(1,1)
+
