6c527c1f988adaf540a54edc1f216131a0f38f87
TemoanaCummings <cummings.temoana@gmail.com>
2022-12-05 18:51:22 +1300

    Attempted better answer

--- kyu8/even-or-odd.js
+++ kyu8/even-or-odd.js
@@ -1,12 +1,18 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 // My Solution:
-function evenOrOdd(number) {
-  if(number % 2 === 0){
-    console.log("Even")
-  }
-  else{
-    console.log("Odd")
-  }
+// function evenOrOdd(number) {
+//   if(number % 2 === 0){
+//     console.log("Even")
+//   }
+//   else{
+//     console.log("Odd")
+//   }
+// }
+// evenOrOdd(2)
+
+// New Solution:
+function evenOrOdd(n){
+  number % 2 === 0 ? "Even" : "Odd";
 }
-evenOrOdd(2)
+evenOrOdd(1)
