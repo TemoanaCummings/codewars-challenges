776dd273e6852d260f0d95cc64375751dc5cbbc1
Low <cummings.temoana@gmail.com>
2023-01-12 11:21:58 +1300

    Finished Challenge - hard

--- kyu8/to-square-root-or-not.js
+++ kyu8/to-square-root-or-not.js
@@ -1,12 +1,23 @@
 // Write a method, that will get an integer array as parameter and will process every number from this array.
-
 // Return a new array with processing every number of the input-array like this:
-
 // If the number has an integer square root, take this, otherwise square the number.
-
 // Example
 // [4,3,9,7,2,1] -> [2,9,3,49,4,1]
 
 function squareOrSquareRoot(array) {
-    return array;  
+    var processed = [];
+
+    for (var i=0; i<array.length; i++){
+      var number = array[i];
+      
+      if (Math.sqrt(number) == Math.floor(Math.sqrt(number))){
+        processed.push(Math.sqrt(number));
+      }
+      else {
+        processed.push(number*number);
+      }
+      
+    }
+    return processed; 
 }
+squareOrSquareRoot([4,3,9,7,2,1]);
