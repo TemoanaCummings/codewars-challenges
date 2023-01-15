c9c48114a7766957be99e479aed25e2f4a06439b
Low <cummings.temoana@gmail.com>
2023-01-16 10:35:21 +1300

    Completed Challenge

--- kyu8/remove-dupelicates-from-list.js
+++ kyu8/remove-dupelicates-from-list.js
@@ -1,8 +1,11 @@
 // Define a function that removes duplicates from an array of numbers and returns it as a result.
-
 // The order of the sequence has to stay the same.
 
 // My Solution:
 function distinct(a) {
-    
+    let unique = a.filter((c,index) => {
+        return a.indexOf(c) === index;
+    })
+    return unique;
 }
+console.log(distinct([1,1,2]))
