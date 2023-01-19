d2b48896d332adab8e4e20a9cd68bdcb3f4a88ef
Low <cummings.temoana@gmail.com>
2023-01-20 11:54:07 +1300

    New Challenge

---
+++ kyu8/switch-it-up.js
@@ -0,0 +1,10 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// My Solution:
+function switchItUp(number){
+    //Write your own Code!
+}
