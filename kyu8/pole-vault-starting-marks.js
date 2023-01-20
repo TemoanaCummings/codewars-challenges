75020436420cda73f7a70d08e45d6286e55a46ec
Low <cummings.temoana@gmail.com>
2023-01-21 12:28:45 +1300

    Finished Challenge - Hard

--- kyu8/pole-vault-starting-marks.js
+++ kyu8/pole-vault-starting-marks.js
@@ -6,12 +6,18 @@
 
 // You will receive a vaulter's height in meters (which will always lie in a range between a minimum of 1.22 meters and a maximum of 2.13 meters). Your job is to return the best starting mark in meters, rounded to two decimal places.
 
-// My Solution:
+// a vs b height difference = 0.31
+// a vs b starting mark difference = 1.22
+// 3.935
+//0.160
+//0.171
+//6.21
 
+// My Solution:
 function startingMark(bodyHeight){
     // Remember: Body height of 1.52 m --> starting mark: 9.45 m
     //           Body height of 1.83 m --> starting mark: 10.67 m
     // All other starting marks are based on these guidelines!
-
-    
+    var m = (10.67 - 9.45) / (1.83 - 1.52);
+    return Math.round((10.67 + m * bodyHeight - m * 1.83) * 100) / 100;
 }
