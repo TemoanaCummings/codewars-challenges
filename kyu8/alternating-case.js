6c884ff14beebd268f150d8e7af66d0ba721226f
Low <cummings.temoana@gmail.com>
2023-01-25 10:21:58 +1300

    Finished Challenge - Hard

--- kyu8/alternating-case.js
+++ kyu8/alternating-case.js
@@ -11,5 +11,14 @@
 // My Solution:
 String.prototype.toAlternatingCase = function () {
     // Define your method here :)
-    
+    let str = "";
+    for(let i = 0; i < this.length; i++){
+        if(this[i] === this[i].toUpperCase()){
+            str += this[i].toLowerCase();
+        }
+        else{
+            str += this[i].toUpperCase();
+        }
+    }
+    return str;
 }
