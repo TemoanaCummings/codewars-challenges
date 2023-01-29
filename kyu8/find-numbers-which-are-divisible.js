07db295e2337e5e5928d842dc6180433017a8a6f
Low <cummings.temoana@gmail.com>
2023-01-30 10:31:13 +1300

    Completed Challenge

--- kyu8/find-numbers-which-are-divisible.js
+++ kyu8/find-numbers-which-are-divisible.js
@@ -5,5 +5,11 @@
 
 // My Solution:
 function divisibleBy(numbers, divisor){
-
+    let result = [];
+    for(let i = 0; i < numbers.length; i ++) {
+        if(numbers[i] % divisor === 0){
+            result.push(numbers[i]);
+        }
+    }
+    return result;
 }
