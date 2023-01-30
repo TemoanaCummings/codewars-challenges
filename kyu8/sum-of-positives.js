5e2571b2bf970ff47f782fe090b5a7154a2ebb6f
Low <cummings.temoana@gmail.com>
2023-01-30 21:05:21 +1300

    Completed Challenge

--- kyu8/sum-of-positives.js
+++ kyu8/sum-of-positives.js
@@ -6,5 +6,12 @@
 
 // My Solution:
 function positiveSum(arr) {
-  
+    let positive = 0;
+    for( let i = 0; i < arr.length; i++)
+    {
+        if(arr[i] > 0){
+            positive += arr[i];
+        }
+    }
+    return positive;
 }
