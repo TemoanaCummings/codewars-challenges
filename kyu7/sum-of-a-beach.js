177337369ab7b9733b37e4a41d604c714558de17
Low <cummings.temoana@gmail.com>
2023-02-01 11:27:36 +1300

    Completed Challenge

--- kyu7/sum-of-a-beach.js
+++ kyu7/sum-of-a-beach.js
@@ -9,4 +9,14 @@
 // My Solution:
 function sumOfABeach(beach) {
     // your code here
+    let result = 0;
+    beach = beach.toLowerCase();
+    let elements = ["sand", "water", "fish", "sun"];
+    for (let i = 0; i < 4; i++) {
+        while (beach.includes(elements[i])) {
+            beach = beach.replace(elements[i], '');
+            result++;
+        }
+    }
+    return result;
 }
