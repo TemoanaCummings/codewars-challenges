e97afde749678deeedb1eaf08fea63d5687d96bd
Low <cummings.temoana@gmail.com>
2023-03-04 17:33:12 +1300

    Completed Challenge

--- kyu7/speed-control.js
+++ kyu7/speed-control.js
@@ -20,6 +20,16 @@
 // Happy coding!
 
 // My Solution:
-function gps(s, x) {
-    // your code
-}
+const gps = (s, x) => {
+
+    if (x.length<=1) {
+      return 0;
+    }
+    
+    let output = [];
+    for (let i = 0; i < x.length-1; i++) { 
+      output.push((x[i+1]-x[i])*3600/s);
+    }
+    
+    return Math.max(...output);
+  }
