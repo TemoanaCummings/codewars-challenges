dc981b39f351081fea15d97986ae1c00d7b487c8
Low <cummings.temoana@gmail.com>
2023-03-05 09:50:43 +1300

    Completed Challenge

--- kyu7/correct-time-string.js
+++ kyu7/correct-time-string.js
@@ -16,5 +16,11 @@
 
 // My Solution:
 function timeCorrect(timestring) {
-    return "?";
-}
+    if (!timestring)
+      return timestring
+      
+    if (!/^(\d\d:){2}\d\d$/.test(timestring))
+      return null
+      
+    return (new Date(0,0,0,...timestring.split(':'))).toString().slice(16,24)
+  }
