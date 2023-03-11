9282b9e4098090a92243e32c02fd0c97e52ad9f6
Low <cummings.temoana@gmail.com>
2023-03-11 16:33:49 +1300

    Completed Challenge

--- kyu7/find-the-vowels.js
+++ kyu7/find-the-vowels.js
@@ -14,4 +14,11 @@
 // My Solution:
 function vowelIndices(word){
   //your code here
+  var arr = [];
+  for(var i = 0; i < word.length; i++) {
+    if(/[aeioyu]/i.test(word[i])) {
+      arr.push(i+1);
+    }
+  }
+  return arr;
 }
