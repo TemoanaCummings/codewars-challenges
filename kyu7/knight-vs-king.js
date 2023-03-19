ccbaba85b763b78e7cbf7fd7079cf74c97e7a3ac
Low <cummings.temoana@gmail.com>
2023-03-19 20:10:37 +1300

    New Challenge

---
+++ kyu7/knight-vs-king.js
@@ -0,0 +1,14 @@
+// You put a Knight and a King in the board.
+
+// Complete the method that tell us which one can capture the other one.
+
+// You are provided with two object array; each contains an integer (the rank, first item) and a string/char (the file, second item) to show the position of the pieces in the chess board.
+
+// Return:
+
+// "Knight" if the knight is putting the king in check,
+// "King" if the king is attacking the knight
+// "None" if none of the above occur
+// Example:
+
+// My Solution:
