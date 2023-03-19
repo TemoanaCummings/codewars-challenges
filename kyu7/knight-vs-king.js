a8d493d2115726fe8e20e622ce9e3724cc7e616b
Low <cummings.temoana@gmail.com>
2023-03-19 20:11:30 +1300

    Completed Challenge

--- kyu7/knight-vs-king.js
+++ kyu7/knight-vs-king.js
@@ -12,3 +12,14 @@
 // Example:
 
 // My Solution:
+function knightVsKing(knightPosition, kingPosition) {
+    var diffX = Math.abs(knightPosition[1].charCodeAt() - kingPosition[1].charCodeAt());
+    var diffY = Math.abs(knightPosition[0] - kingPosition[0]);
+    
+    if (diffX <= 1 && diffY <=1) {
+      return 'King'
+    } else if (diffX == 1 && diffY == 2 || diffX == 2 && diffY == 1) {
+      return 'Knight'
+    } 
+    return 'None';
+  }
